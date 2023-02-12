87fb5d4530eb8470446ebb0708e45eed43cec270
Nathaniel Pather <nathaniel_pather@hotmail.com>
2023-02-12 17:55:24 +1000

    Remove references of weather controller and model

--- ToDo.API/Controllers/WeatherForecastController.cs
+++ ToDo.API/Controllers/WeatherForecastController.cs
@@ -4,29 +4,16 @@ namespace ToDo.API.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class WeatherForecastController : ControllerBase
+	public class ToDoListController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
+		public ToDoListController()
 		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
-
-		private readonly ILogger<WeatherForecastController> _logger;
-
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
-		{
-			_logger = logger;
 		}
 
-		[HttpGet(Name = "GetWeatherForecast")]
-		public IEnumerable<WeatherForecast> Get()
+		[HttpGet]
+		public IActionResult Get()
 		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = Random.Shared.Next(-20, 55),
-				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-			})
-			.ToArray();
+			return Ok("I passed");
 		}
 	}
 }
