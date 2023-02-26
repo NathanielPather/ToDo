2355ad69199b37efd33a36983d0059fed418bcbf
Nathaniel Pather <Nathaniel.Pather@riotinto.com>
2023-02-27 09:40:40 +1000

    Add colour themes file

---
+++ ToDo.UI/src/theme.ts
@@ -0,0 +1,9 @@
+﻿/* Main Colours */
+const BLUE_MAIN =  "#C3DDF7";
+const RED_MAIN =  "#F7C3DD";
+const GREEN_MAIN =  "#DDF7C3";
+
+/* Border Colours */
+const GREEN_BORDER = "#C4F196";
+const BLUE_BORDER = "#96C4F1";
+const RED_BORDER = "#F196C4";
