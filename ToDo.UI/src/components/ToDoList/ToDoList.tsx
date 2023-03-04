8e08c5020b0876be77eceb0d2586b73d8b616d31
Nathaniel Pather <nathaniel_pather@hotmail.com>
2023-03-04 16:13:38 +1000

    Add ToDoList component

---
+++ ToDo.UI/src/components/ToDoList/ToDoList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ToDoListStyles.css";
+
+function ToDoList() {
+  return(
+    <div className="ToDoList">
+      
+    </div>
+  )
+}
+
+export default ToDoList;
