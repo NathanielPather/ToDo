4daf032a6473a925f1ac5fd46baea023c36a1b5f
Nathaniel Pather <Nathaniel.Pather@riotinto.com>
2023-05-16 14:08:55 +1000

    Include added ToDoList information on page display

--- ToDo.UI/src/app/components/ToDoLists.tsx
+++ ToDo.UI/src/app/components/ToDoLists.tsx
@@ -4,13 +4,16 @@ import { GetLists } from "../api/api";
 
 function CreateToDoListElements() {
 	let HTMLElement: React.JSX.Element[] = [];
-	const lists = GetLists();
+	const lists: ToDoList[] = GetLists();
 
 	if(lists.length > 0) {
 		lists.map((list, index) => {
-			HTMLElement.push(<div key={index}>{list.name}</div>);
+			HTMLElement.push(
+				<div key={index}>
+					{list.name} {list.completed}/{list.totalTasks} {list.resetTimer}
+				</div>);
 		});
-		
+
 		return HTMLElement;
 	}
 
