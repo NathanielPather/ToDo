4cacab13f80a2554f1416d33243dc0bf6d83af29
Nathaniel Pather <Nathaniel.Pather@riotinto.com>
2023-05-16 14:08:18 +1000

    Fix fake api to use ToDoList interface

--- ToDo.UI/src/app/api/api.ts
+++ ToDo.UI/src/app/api/api.ts
@@ -1,12 +1,23 @@
-const lists = [
+
+
+const lists: ToDoList[] = [
 	{ 
-		name: "work"
+		name: "work",
+		completed: 2,
+		totalTasks: 5,
+		resetTimer: "13:23:25"
 	},
 	{ 
-		name: "Music"
+		name: "Music",
+		completed: 2,
+		totalTasks: 5,
+		resetTimer: "13:23:25"
 	},
 	{ 
-		name: "League"
+		name: "League",
+		completed: 2,
+		totalTasks: 5,
+		resetTimer: "13:23:25"
 	},
 ]
 
