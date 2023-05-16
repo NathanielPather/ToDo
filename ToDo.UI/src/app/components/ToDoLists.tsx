c92ab0ad6902464d2047a48f730c057bb22fbb9e
Nathaniel Pather <Nathaniel.Pather@riotinto.com>
2023-05-17 09:43:59 +1000

    Add usage of theme config with theme provider for ToDoList component

--- ToDo.UI/src/app/components/ToDoLists.tsx
+++ ToDo.UI/src/app/components/ToDoLists.tsx
@@ -3,6 +3,8 @@ import Accordion from "@mui/material/Accordion";
 import { GetLists } from "../api/api";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../themes/theme";
 
 function CreateToDoListElements() {
 	let HTMLElement: React.JSX.Element[] = [];
@@ -11,16 +13,19 @@ function CreateToDoListElements() {
 	if (lists.length > 0) {
 		lists.map((list) => {
 			HTMLElement.push(
-			<Accordion>
-				<AccordionSummary
-					aria-controls="panel1a-content"
-					id="panel1a-header">
-					{list.name} {list.completed}/{list.totalTasks} {list.resetTimer}
-				</AccordionSummary>
-				<AccordionDetails>
-					TODO
-				</AccordionDetails>
-			</Accordion>);
+				<ThemeProvider theme={theme}>
+					<Accordion>
+						<AccordionSummary
+							aria-controls="panel1a-content"
+							id="panel1a-header">
+							{list.name} {list.completed}/{list.totalTasks} {list.resetTimer}
+						</AccordionSummary>
+						<AccordionDetails>
+							TODO
+						</AccordionDetails>
+					</Accordion>
+				</ThemeProvider>
+			);
 		});
 
 		return HTMLElement;
