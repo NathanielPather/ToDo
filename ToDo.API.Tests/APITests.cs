d3a4376a7f2fb5173d8eb669d76c8cf69b9adf82
Nathaniel Pather <nathaniel_pather@hotmail.com>
2023-08-03 19:50:36 +1000

    Add partial api test

---
+++ ToDo.API.Tests/APITests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using ToDo.API.Controllers;
+using ToDo.Core.Interfaces;
+using Xunit;
+
+namespace ToDo.API.Tests
+{
+    public class APITests
+    {
+        [Fact]
+        public async Task GetAllToDoLists()
+        {
+            // arrange
+            var toDoListService = new Mock<IToDoListService>();
+            toDoListService.Setup(x => x.GetToDoList()).Returns(ToDoMockData.GetToDoList().Any());
+            var sut = new ToDoListController(toDoListService.Object);
+
+            /* https://www.learmoreseekmore.com/2022/02/dotnet6-unit-testing-aspnetcore-web-api-using-xunit.html */
+
+            // act
+            // assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ ToDo.API.Tests/ToDoMockData.cs
@@ -0,0 +1,25 @@
+﻿using ToDo.Core.Models;
+
+namespace ToDo.API.Tests
+{
+    public class ToDoMockData
+    {
+        public static List<ToDoList> GetToDoList()
+        {
+            return new List<ToDoList> {
+                new ToDoList {
+                    Name = "Work"
+                },
+                new ToDoList {
+                    Name = "Chores"
+                },
+                new ToDoList {
+                    Name = "Piano"
+                },
+                new ToDoList {
+                    Name = "League of Legends"
+                }
+            };
+        }
+    }
+}
