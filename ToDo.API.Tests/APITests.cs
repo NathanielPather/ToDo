437a9a713257f235629735f0b779aae6f0db8d24
Nathaniel Pather <nathaniel_pather@hotmail.com>
2023-08-18 07:31:20 +1000

    API-Tests

--- ToDo.API.Tests/APITests.cs
+++ ToDo.API.Tests/APITests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using ToDo.API.Controllers;
 using ToDo.Core.Interfaces;
 using Xunit;
@@ -12,14 +13,23 @@ namespace ToDo.API.Tests
         {
             // arrange
             var toDoListService = new Mock<IToDoListService>();
-            toDoListService.Setup(x => x.GetToDoList()).Returns(ToDoMockData.GetToDoList().Any());
+            toDoListService.Setup(x => x.GetToDoList()).Returns(ToDoMockData.GetToDoList());
             var sut = new ToDoListController(toDoListService.Object);
 
             /* https://www.learmoreseekmore.com/2022/02/dotnet6-unit-testing-aspnetcore-web-api-using-xunit.html */
 
             // act
+            var result = (OkObjectResult)await sut.GetAllToDoLists();
+
             // assert
-            Assert.True(true);
+            Assert.True(result.StatusCode.Equals(200));
+
+            // should all apis return a status code
+                // yes, bool is pointless as status code provides more info
+            // should all apis be async
+                // no, depends on requirements, minimal processing should not be async
+            // should apis return an object/list of objects
+                // depends on what you want
         }
     }
 }
